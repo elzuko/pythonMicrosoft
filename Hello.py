1218dbacd76d255289b2da51e45c09e2251fed62
elzuko <bcld1972@gmail.com>
2023-11-24 12:25:05 -0300

    1

--- Hello.py
+++ Hello.py
@@ -1,1 +1,13 @@
 print('Hello, World!')
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+
+planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
+distance_to_alpha_centauri = 4.367 # float, lightyears
+can_liftoff = True
+shuttle_landed_on_the_moon = "Apollo 11" #string
+
+print(type (planets_in_solar_system))
+print(type (distance_to_alpha_centauri))
+print(type (can_liftoff))
